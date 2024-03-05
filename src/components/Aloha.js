34c82fd6bb774119365c58c065235ad851f5a3ca
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-05 20:26:13 +0200

    Sort Users

--- src/components/Aloha.js
+++ src/components/Aloha.js
@@ -3,19 +3,19 @@ import React from "react";
 class Aloha extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
-            // 1. Створити зміну тексту (за аналогією з лампочкою)
-            text: true
+            // 1. Створити змінну стейту (за аналогією з лампочкою)
+            isGreeting: true
         }
     }
 
     // 2. Зробити обробник події кліку на h1 (за аналогією з лампочкою)
 
-    textHandler() {
+    clickHandler() {
         this.setState({
-            text: !this.state.text
-        })
+            isGreeting: !this.state.isGreeting
+        });
     }
 
     // тут можуть бути якісь обробники подій
@@ -23,11 +23,17 @@ class Aloha extends React.Component {
     render() {
         // 3. В залежності від значення стейту, вирахувати що за текст ви показуєте (за аналогією з лампочкою)
 
-        const textForH1 = this.state.text ? 'Hello' : 'Bye';
+        const greetingText = this.state.isGreeting ? 'Hello' : 'Bye';
 
-        return <h1 onClick={() => {this.textHandler()}}>{textForH1} {this.props.name}</h1>
-    } 
+        return <h1 onClick={() => {this.clickHandler()}}>{greetingText} {this.props.firstName} {this.props.lastName}</h1>
+        // P.S. Не забудьте підписати h1 на обробник кліку, розроблений у п.2
+    }
 }
 
 export default Aloha;
 
+/*
+
+Доробити компоненту таким чином, щоб за натиснення на h1 текст "Hello" змінювався на "Bye"
+
+*/
--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -2,16 +2,60 @@ import React from "react";
 import Aloha from "../Aloha";
 
 class AlohaDashboard extends React.Component {
-  render() {
-    return (
-      <>
-        <Aloha name="Alex" />
-        <Aloha name="John" />
-        <Aloha name="Simon" />
-        <Aloha name="Draco" />
-      </>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            usersArray: [
+                {
+                    firstName: 'Tony',
+                    lastName: 'Stark'
+                },
+                {
+                    firstName: 'Bruce',
+                    lastName: 'Brenner'
+                },
+                {
+                    firstName: 'Thor',
+                    lastName: 'Odinson'
+                },
+                {
+                    firstName: 'Peter',
+                    lastName: 'Parker'
+                },
+                {
+                    firstName: 'Natasha',
+                    lastName: 'Romanof'
+                },
+            ]
+        }
+    }
+    
+    userToAloha() {
+        const { usersArray } = this.state;
+        return usersArray.map((user) => <Aloha firstName={user.firstName} lastName={user.lastName} />)
+    }
+
+    sortUsers() {
+        const { usersArray } = this.state;
+
+        const newArray = [...usersArray];
+
+        newArray.sort((a, b) => a.firstName > b.firstName ? 1 : -1);
+
+        this.setState({
+            usersArray: newArray
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <button onClick={() => {this.sortUsers()}}>Sort</button>
+                {this.userToAloha()}
+            </>
+        )
+    }
 }
 
 export default AlohaDashboard;
