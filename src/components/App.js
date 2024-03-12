bde0577d2402e5ef0074b8cbc7feb1643ae64ef8
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-12 18:30:13 +0200

    ParentForm practice task

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SignInForm from "./SingInForm/SignInForm";
 import GreetingsForm from "./GreetingsForm/GreetingsForm";
+import HelloForm from "./HelloForm/HelloForm";
+import HelloParent from "./HelloForm/HelloParent";
 
 function App() {
     return (
-        <GreetingsForm />
+        <HelloParent />
     )
 }
 
