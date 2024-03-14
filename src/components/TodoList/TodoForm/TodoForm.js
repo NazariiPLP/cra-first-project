d873f788df9506a567c0452a2ab1eeae9c8a0355
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-14 19:00:36 +0200

    Css modules

--- src/components/TodoList/TodoForm/TodoForm.js
+++ src/components/TodoList/TodoForm/TodoForm.js
@@ -1,6 +1,7 @@
 // TodoForm - класова компонента
 
 import React, { Component } from 'react';
+import styles from './TodoFormStyle.module.css';
 
 class TodoForm extends Component {
     constructor(props) {
@@ -37,7 +38,7 @@ class TodoForm extends Component {
         const { taskText } = this.state;
 
         return (
-            <form onSubmit={this.submitHandler}>
+            <form onSubmit={this.submitHandler} className={styles.container}>
                 <input type='text' value={taskText} name='taskText' onChange={this.changeHandler}/> {/* 3. Потрібно вказати, що value інпута контролюється стейтом */}
                 <button type='Submit'>Add task</button>
             </form>
