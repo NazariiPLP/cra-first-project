766346237bf95db7f75078fa588405d86f59f5e3
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-22 19:28:37 +0200

    UserHOC + home task

--- src/components/Tree/Parent/Subparent/Child/Child.js
+++ src/components/Tree/Parent/Subparent/Child/Child.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import InnerChild from './InnerChild/InnerChild';
-import { UserContext } from '../../../../../contexts/userContext';
+import { withUser } from '../../../../../HOC';
 import styles from './Child.module.css';
 
 const Child = (props) => {
     return (
-        <UserContext.Consumer>
-            {({user }) => {
-                return (
-                    <div className={styles.border}>
-                        Child
-                        <img className={styles.img} src={user.avatar} alt={`${user.firstName} ${user.lastName}`} />
-                        <InnerChild />
-                    </div>
-                )
-            }}
-        </UserContext.Consumer>
-    );
+        <div className={styles.border}>
+            Child
+            <img className={styles.img} src={props.user.avatar} alt={`${props.user.firstName} ${props.user.lastName}`} />
+            <InnerChild />
+        </div>
+    )
 }
 
-export default Child;
+export default withUser(Child);
+
+/*
+
++В компоненті Child відобразити або картинку або прізвище / ім’я
+
+Декомпозиція задачі:
+
++ 1. Підписатись Consumer до контексту юзера (UserContext)
++2. В рендер-пропі Consumer витягнути об'єкт юзера і відобразити необхідну інформацію 
+
+*/
