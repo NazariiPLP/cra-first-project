51b2dec5c7db8a7c9664ddcaeeba0c8c4475a112
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-22 18:09:19 +0200

    Context practice task

--- src/components/Tree/Parent/Subparent/Child/Child.js
+++ src/components/Tree/Parent/Subparent/Child/Child.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import InnerChild from './InnerChild/InnerChild';
+import { UserContext } from '../../../../../contexts/userContext';
+import styles from './Child.module.css';
 
 const Child = (props) => {
     return (
-        <div>
-            Child
-            <InnerChild />
-        </div>
+        <UserContext.Consumer>
+            {({user }) => {
+                return (
+                    <div className={styles.border}>
+                        Child
+                        <img className={styles.img} src={user.avatar} alt={`${user.firstName} ${user.lastName}`} />
+                        <InnerChild />
+                    </div>
+                )
+            }}
+        </UserContext.Consumer>
     );
 }
 
