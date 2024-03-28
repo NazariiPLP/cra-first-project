5c544bf9c017711c45bb4566e131995790bcfdae
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-28 18:49:36 +0200

    Practice task, useContext

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
 import Parent from './Parent/Parent';
 import style from './Tree.module.css';
 import CONSTANTS from '../../constants';
 import cx from 'classnames';
-import { withTheme } from '../../HOC';
-
+import { ThemeContext } from '../../contexts/themeContext';
 const { THEMES } = CONSTANTS;
 
 
 const Tree = (props) => {
+    // 1. Використати useContext (дістати з контексту теми поле theme)
+    const {theme}= useContext(ThemeContext);
+    // 2. По коду компоненти внести правки
+    // 3. Виправити експорт у цьому файлі
+
     const classNames = cx(style.container, {
-        [style.lightTheme]: props.theme === THEMES.LIGHT,
-        [style.darkTheme]: props.theme === THEMES.DARK
+        [style.lightTheme]: theme === THEMES.LIGHT,
+        [style.darkTheme]: theme === THEMES.DARK
     });
 
     return (
@@ -22,6 +26,4 @@ const Tree = (props) => {
     )
 }
 
-const TreeWithTheme = withTheme(Tree);
-
-export default TreeWithTheme;
+export default Tree;
