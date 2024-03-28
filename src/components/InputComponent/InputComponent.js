033238b8d1f9315a01274f64ae5a258911f163ab
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-28 19:54:14 +0200

    Practice task input focus

---
+++ src/components/InputComponent/InputComponent.js
@@ -0,0 +1,40 @@
+/*
+
+Є компонента з інпутом.
+При завантаженні компоненти - сфокусуйтесь на інпуті.
+Використайте для цього реф.
+
+Підказка: 
+
+Ви десь створюєте реф: inputRef
+
+useEffect(() => { // ComponentDidMount
+    //сфокусуватись на інпуті під час завантаження компоненти
+    inputRef.current.focus();
+}, []);
+
+
+*/
+
+import { useRef, useEffect } from 'react';
+
+const InputComponent = () => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
+    return (
+        <>
+            <input 
+            ref={inputRef}
+            type='text'
+            placeholder='Введіть тут щось...'
+
+            />
+        </>
+    );
+}
+
+export default InputComponent;
