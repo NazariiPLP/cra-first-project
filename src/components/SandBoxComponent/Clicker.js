87aa1348a0f24dc58e05bc728dc01d20b0988d0a
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-29 19:22:44 +0200

    UseCallback practice task

--- src/components/SandBoxComponent/Clicker.js
+++ src/components/SandBoxComponent/Clicker.js
@@ -4,21 +4,44 @@ import { useState, useCallback } from 'react';
 
 const Clicker = () => {
     const [clickCount, setClickCount] = useState(0);
-    
-    // const clickHandler = () => {
-    //     setClickCount(clickCount + 1);
-    // }
+    const [inputValue, setInputValue] = useState();
 
     const clickHandler = useCallback(() => {
         setClickCount(clickCount => clickCount + 1);
     }, []);
 
+    const changeHandler = useCallback(({ target: { value } }) => {
+        setInputValue(Number(value));
+    }, []);
+
+    const logValueHandler = useCallback(() => {
+        let sum = 0;
+        for(let i = 0; i <= inputValue; i++){
+            sum += i;
+        }
+
+        console.log(sum);
+        console.log(inputValue);
+    }, [inputValue]);
+
     return (
         <>
             <h1>{clickCount}</h1>
             <button onClick={clickHandler}>Click</button>   
+            <input type='text' value={inputValue} onChange={changeHandler} placeholder='Введіть число, для якого потрібно знайти суму (верхню межу)'/>
+            <button onClick={logValueHandler}>Розрахувати суму</button>
         </>
     );  
 }
 
 export default Clicker;
+
+
+/*
+
+Використайте хук useCallback для оптимізації.
+Огорніть useValueHandler та changeHandler у useCallback.
+Використайте вже мемоізовану функцію.
+
+
+*/
