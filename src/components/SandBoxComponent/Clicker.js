2a6dac174655453dc746e945c27cec62e2ee6dfb
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-29 19:38:07 +0200

    Hook useMemo

--- src/components/SandBoxComponent/Clicker.js
+++ src/components/SandBoxComponent/Clicker.js
@@ -1,10 +1,10 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 
 //Задача: є кнопка, підрахуваи кількість натиснень на неї
 
 const Clicker = () => {
     const [clickCount, setClickCount] = useState(0);
-    const [inputValue, setInputValue] = useState();
+    const [inputValue, setInputValue] = useState('');
 
     const clickHandler = useCallback(() => {
         setClickCount(clickCount => clickCount + 1);
@@ -14,15 +14,18 @@ const Clicker = () => {
         setInputValue(Number(value));
     }, []);
 
-    const logValueHandler = useCallback(() => {
+    const logValueHandler = () => {
         let sum = 0;
-        for(let i = 0; i <= inputValue; i++){
+        for(let i = 0; i < inputValue; i++){
             sum += i;
         }
 
-        console.log(sum);
-        console.log(inputValue);
-    }, [inputValue]);
+        return sum;
+    };
+
+    const memoizedValue = useMemo(() => logValueHandler(), [inputValue]);
+    
+    console.log(memoizedValue);
 
     return (
         <>
