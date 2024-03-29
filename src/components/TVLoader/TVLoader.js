180391be5f9f3cd920cb0641cf50e0a758fe6c6c
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-29 20:48:53 +0200

    Pt index added

--- src/components/TVLoader/TVLoader.js
+++ src/components/TVLoader/TVLoader.js
@@ -1,58 +1,6 @@
 import React from 'react';
 import useData from '../DataProvider/DataProvider';
 
-// class TVLoader extends Component {
-//     constructor(props) {
-//         super(props);
-        
-//         this.state = {
-//             tv: [],
-//             isLoading: true,
-//             isError: false
-//         }
-//     }
-
-//     componentDidMount() {
-//         this.load();
-//     }
-
-//     load = () => {
-//         fetch('./tv.json')
-//         .then((response) => response.json())
-//         .then((tv) => {
-//             this.setState({
-//                 tv
-//             });
-//         })
-//         .catch((error) => {
-//             this.setState({
-//                 isError: error
-//             })
-//         })
-//         .finally(() => {
-//             this.setState({
-//                 isLoading: false
-//             });
-//         });
-//     }
-
-//     render() {
-//         const { tv, isLoading, isError } = this.state;
-
-//         return (
-//             <>
-//                 {isLoading && <div>Loading...</div>}
-//                 {isError && <div>Error happaned: {isError.massage}!</div>}
-
-//                 <ul>
-//                     {tv.map((currentTV) => <li>Brand: {currentTV.brand} --- Model: {currentTV.model} --- Price: {currentTV.price}</li>)}
-//                 </ul>
-//             </>
-//         );
-//     }
-// }
-
-
 const TVLoader = () => {
     const {data: tv, isLoading, error } = useData(getTV);
 
@@ -67,7 +15,7 @@ const TVLoader = () => {
             {error && <div>Error happaned: {error.massage}!</div>}
             
             <ul>
-                {tv.map((currentTV) => <li>Brand: {currentTV.brand} --- Model: {currentTV.model} --- Price: {currentTV.price}</li>)}
+                {tv.map((currentTV, index) => <li key={index}>Brand: {currentTV.brand} --- Model: {currentTV.model} --- Price: {currentTV.price}</li>)}
             </ul>
         </>
     );
